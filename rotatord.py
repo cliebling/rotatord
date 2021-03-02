eb49a40080dcd7b706b65c6762571644b1dc8ccc
chuck <chuck@vulpes.foxyfluffs.org>
2021-03-02 14:27:56 -0600

    Added license info

--- rotatord.py
+++ rotatord.py
@@ -1,4 +1,26 @@
 #!/usr/bin/python 
+"""
+rotatord.py
+Implements a rot2prog compliant antenna az/el rotator
+on a Raspberry Pi
+
+Copyright (C) 2020 Charles R. Liebling
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+"""
+
 from __future__ import division
 import serial
 import getopt
@@ -7,9 +29,6 @@ import time
 import RPi.GPIO as gpio
 from Queue import Queue
 from threading import Thread
-
-# Rot2Prog implementation (c) 2020 C. Liebling W3PZ crlieb@swbell.net
-# Distributed under the GPLv2
 
 CmdStop = 0x0F
 CmdStatus = 0x1F
